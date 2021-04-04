e45e03ddb93826bc8a4f813e5a37d7894e0ef076
DongKyu Kim <aass0903@naver.com>
2021-04-04 18:05:20 +0900

    update sdk version

--- app/src/main/java/kr/ac/cau/easyconnect/MainActivity.kt
+++ app/src/main/java/kr/ac/cau/easyconnect/MainActivity.kt
@@ -1,11 +1,25 @@
 package kr.ac.cau.easyconnect
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.os.Build
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
 }
