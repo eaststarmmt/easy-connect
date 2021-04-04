bb875a726a7b968924d505f87e6e5c9eab0f4cbd
DongKyu Kim <aass0903@naver.com>
2021-04-04 22:56:51 +0900

    implement of STT

--- app/src/main/java/kr/ac/cau/easyconnect/MainActivity.kt
+++ app/src/main/java/kr/ac/cau/easyconnect/MainActivity.kt
@@ -1,25 +1,59 @@
 package kr.ac.cau.easyconnect
 
 import android.Manifest
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.app.Activity
 import android.content.Intent
 import android.os.Build
-import android.speech.RecognitionListener
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
+
 class MainActivity : AppCompatActivity() {
+
+    private var speechRecognizer: SpeechRecognizer? = null
+    private var REQUEST_CODE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (Build.VERSION.SDK_INT >= 23)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.INTERNET, Manifest.permission.RECORD_AUDIO), REQUEST_CODE)
+
+        UseActivityResultSTTButton.setOnClickListener { startSTTUseActivityResult() }
     }
 
+    private fun startSTTUseActivityResult() {
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
+
+        val speechRecognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+            putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, packageName)
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+        }
+
+        startActivityForResult(speechRecognizerIntent, 100)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode == Activity.RESULT_OK && requestCode == 100) {
+            viewText.text = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)!![0]
+        }
+    }
+
+    override fun onDestroy() {
+        if (speechRecognizer != null) {
+            speechRecognizer!!.stopListening()
+        }
+        super.onDestroy()
+    }
 }
