0d1462a6fc998848a638f621473b2bdeeb91002b
DongKyu Kim <aass0903@naver.com>
2021-04-11 21:49:07 +0900

    add board branch

--- app/src/main/java/kr/ac/cau/easyconnect/MainActivity.kt
+++ app/src/main/java/kr/ac/cau/easyconnect/MainActivity.kt
@@ -13,13 +13,16 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
-import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.boardButton).setOnClickListener{
+            var intent = Intent(this, BoardActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
