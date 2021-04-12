ebdad3e20cdb950bb57fd3cb5505c5c9de10448b
DongKyu Kim <aass0903@naver.com>
2021-04-12 12:56:40 +0900

    update board

--- app/src/main/java/kr/ac/cau/easyconnect/BoardActivity.kt
+++ app/src/main/java/kr/ac/cau/easyconnect/BoardActivity.kt
@@ -1,11 +1,17 @@
 package kr.ac.cau.easyconnect
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class BoardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_board)
+        findViewById<Button>(R.id.create).setOnClickListener{
+            var intent = Intent(this, PostActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/kr/ac/cau/easyconnect/Post.kt
@@ -0,0 +1,11 @@
+package kr.ac.cau.easyconnect
+
+class Post {
+    var title: String
+    var content: String
+
+    constructor(title: String, content: String) {
+        this.title = title
+        this.content = content
+    }
+}
