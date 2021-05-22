bb2527daadc8f01202ce453f6ec158029481de87
DongKyu Kim <aass0903@naver.com>
2021-05-23 06:30:53 +0900

    implement of reply without emoticon

--- app/src/main/java/kr/ac/cau/easyconnect/DetailMainActivity.kt
+++ app/src/main/java/kr/ac/cau/easyconnect/DetailMainActivity.kt
@@ -175,38 +175,7 @@ class DetailMainActivity : AppCompatActivity() {
             dialog.show()
 
         }
-      /*
-        //댓글 등록시
-        findViewById<Button>(R.id.reply_button).setOnClickListener {
 
-            var inputReply = replyContent.text.trim().toString()
-            var name = firebaseAuth!!.currentUser.email
-            // 현재 시간 출력
-            val currentDateTime : Long  = System.currentTimeMillis()
-            var registered : Long = System.currentTimeMillis()
-            var modified : String = SimpleDateFormat("MM월dd일 HH:mm:ss").format(currentDateTime)
-
-
-            val replyDTO : ReplyDTO = ReplyDTO(inputReply, name, registered, modified)
-
-            if (inputReply.isNullOrEmpty()) {
-                var builder = AlertDialog.Builder(this)
-                builder.setTitle("내용을 입력해주세요.")
-                builder.setPositiveButton("확인", null)
-                builder.show()
-            } else {
-                db.collection("post/" + thisData!!.registered.toString() + "/reply").document(registered.toString()).set(replyDTO).addOnCompleteListener(this) {
-                    //글이 정상적으로 작성 됐을 때
-                    if (it.isSuccessful) {
-                        Toast.makeText(this, "완료", Toast.LENGTH_SHORT).show()
-                    } else {
-                        Toast.makeText(this, "failed", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }
-
-        }
-        */
         // 댓글보기. 일단 액티비티로 해둠
         findViewById<TextView>(R.id.replyList).setOnClickListener {
             val intent = Intent(this, ReplyActivity::class.java)
